86fffac5a6154fdd023dfe7185a251afe88ec72b
MP <pavlicic1marko@gmail.com>
2024-12-16 13:25:18 +0100

    adding login page, with login form

--- auth_app/authenticate/views.py
+++ auth_app/authenticate/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login ,logout
 
 def home(reqeuest):
     return render(reqeuest, 'authenticate/home.html', {})
+
+def login_user(request):
+    return render(request, 'authenticate/login.html',{})
