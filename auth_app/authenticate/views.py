dbd9b5d9e2e9be066636c0f4b24b968bb3e15325
MP <pavlicic1marko@gmail.com>
2024-12-16 14:58:48 +0100

    adding login logic, and login messages

--- auth_app/authenticate/views.py
+++ auth_app/authenticate/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login ,logout
+from django.contrib import messages
 
 def home(reqeuest):
     return render(reqeuest, 'authenticate/home.html', {})
 
 def login_user(request):
-    return render(request, 'authenticate/login.html',{})
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,('You Have been logged in!'))
+            return redirect('home')
+        else:
+            messages.error(request,('Error'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html',{})
